c41563653047ae27f073854c4a0b9cb3f23964f5
Pav <Pav366spam@gmail.com>
2020-10-21 12:46:57 +0200

    Add Display Tests

---
+++ LadeskabTest/DisplayTests.cs
@@ -0,0 +1,21 @@
+﻿using Library;
+using NUnit.Framework;
+using UsbSimulator;
+
+namespace Ladeskab.Test
+{
+    public class DisplayTestClass
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var n = new ChargeControl(new UsbChargerSimulator());
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
