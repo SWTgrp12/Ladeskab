e2a1fea2cceca133fada8c6c57214a50f581c9f3
Pav <Pav366spam@gmail.com>
2020-10-28 14:19:10 +0100

    Done Coverage

--- LadeskabTest/StationControlTest.cs
+++ LadeskabTest/StationControlTest.cs
@@ -83,15 +83,20 @@ namespace LadeskabTest
             // assert nothing's changed
         }
         // TODO: Needs specific test cases
-        [TestCase(1,1)]
-        [TestCase(1000,1000)]
-        public void TestRfidOpenNoConnection(int id)
+        [TestCase(1, 1)]
+        [TestCase(1000, 1000)]
+        public void TestRfidOpenNoConnection(int OldId,int id)
         {
-            station.State = StationControl.LadeskabState.DoorOpen;
-            chargeControl.connection_establishment().Returns(false);
+            // set old_id
+            station.State = StationControl.LadeskabState.Available;
+            chargeControl.connection_establishment().Returns(false); 
+            rfidreader.RfidHandler += Raise.EventWith(new RfidEventArgs(OldId));
+
             rfidreader.RfidHandler += Raise.EventWith(new RfidEventArgs(id));
 
-            Assert.AreEqual(StationControl.LadeskabState.DoorOpen, station.State);
+            // assert if old_id should match or not
+            Assert.AreNotEqual(station._oldId, id);
+            Assert.AreEqual(StationControl.LadeskabState.Available, station.State);
         }
         // TODO: Needs specific test cases
         [TestCase(1, 1)]
