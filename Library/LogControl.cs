47f0e0331a18f30dd70094361e34caa8a510090c
Pav <Pav366spam@gmail.com>
2020-11-12 16:18:58 +0100

    Changing stationControl tests to be blackbox testing

---
+++ Library/LogControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class LogControl
+    {
+
+        string _fileName = "logfile.txt";
+        string _adress = "";
+
+    }
+        public void write()
+        {
+            using (var writer = File.AppendText(logFile))
+            {
+                writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", e.id_);
+            }
+        }
+    
+}
